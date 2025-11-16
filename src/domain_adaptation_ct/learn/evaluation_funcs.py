479de8f20c9faddbdfeab6c977de66f95ba5f547
Justin Cheung <jrcheung168@gmail.com>
2025-11-16 19:42:57 +0000

    Make eval script less memory hungry

--- src/domain_adaptation_ct/learn/evaluation_funcs.py
+++ src/domain_adaptation_ct/learn/evaluation_funcs.py
@@ -75,18 +75,10 @@ def run_evaluation_from_config_file(config_file: str):
 
     init_logging(logging_dir=output_dir)
 
-    # Load the dataset files.
+    # Decide the dataset files.
     dataset_cls = DATASET_REGISTRY[cfg["evaluation"]["dataset"]["cls_name"]]
     fold_file_paths = cfg["evaluation"]["dataset"]["fold_file_paths"]
     assert isinstance(fold_file_paths, list)
-    fold_datasets = []
-    for fold_file_path in fold_file_paths:
-        fold_datasets.append(
-            dataset_cls.load(
-                file_path = fold_file_path,
-                convert_grayscale_to_rgb = cfg["evaluation"]["dataset"]["convert_grayscale_to_rgb"]
-            )
-        )
 
     # Support running the same evaluation with many models
     if "checkpoint_dirs" in cfg:
@@ -98,18 +90,18 @@ def run_evaluation_from_config_file(config_file: str):
         # Instantiate the model.
         architecture_cls = ARCHITECTURE_REGISTRY[cfg["architecture"]["cls_name"]]
         model = architecture_cls.load(checkpoint_dir, **cfg["architecture"]["cls_init_args"])
-        logging.info(f"Instantiated model {architecture_cls.__name__}. Summary:\n{model}")
+        logging.info(f"Instantiated model {architecture_cls.__name__} from {checkpoint_dir}. Summary:\n{model}")
 
-        # Assume we only look at one fold at a time. This design needs to be reworked to be less convoluted (TODO)
-        folds: list[dict[str, list[int]]] = [{"val": [fold_num]} for fold_num in range(len(fold_file_paths))]
-        for fold_num in range(len(folds)):
-            logging.info(f"Beginning evaluation of fold {fold_num} (#{fold_num+1} out of {len(folds)})")
+        for fold_num in range(len(fold_file_paths)):
+            logging.info(f"Beginning evaluation of fold {fold_num} (#{fold_num+1} out of {len(fold_file_paths)})")
 
-            # Read which folds comprise the validation dataset.
-            val_folds = folds[fold_num]["val"]
-            val_fold_datasets = [fold_datasets[val_fold] for val_fold in val_folds]
-            eval_dataset = MultifoldDataset(datasets = val_fold_datasets)
-            logging.info(f"Instantiated eval dataset {dataset_cls.__name__}, length {len(eval_dataset)}, comprised of files {[fold_file_paths[val_fold] for val_fold in val_folds]}")
+            # Assume we only evaluate one fold at a time.
+            # Load only the dataset needed right now.
+            eval_dataset = dataset_cls.load(
+                file_path = fold_file_paths[fold_num],
+                convert_grayscale_to_rgb = cfg["evaluation"]["dataset"]["convert_grayscale_to_rgb"]
+            )
+            logging.info(f"Instantiated eval dataset {dataset_cls.__name__}, length {len(eval_dataset)}, which is file {fold_file_paths[fold_num]}")
 
             evaluator_cls = TRAINER_REGISTRY[cfg["evaluation"]["evaluator"]["cls_name"]]
 
@@ -121,5 +113,8 @@ def run_evaluation_from_config_file(config_file: str):
                 batch_size=cfg["evaluation"]["evaluation_arguments"]["batch_size"],
                 fold_num=fold_num,
             )
+
+            del eval_dataset
+        del model
 
     # TODO - add some logic to merge the resulting CSV's. may want this to just be a separate script which takes all the paths this sent the CSV's to.
